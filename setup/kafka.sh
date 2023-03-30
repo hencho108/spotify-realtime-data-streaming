fe41d3d073735f3a24a81150e5ea4ffc90c8eec2
Hendrik <you@example.com>
2023-03-30 21:05:31 +0200

    commenting out non-essentials in the kafka server setup script

--- setup/kafka.sh
+++ setup/kafka.sh
@@ -4,15 +4,15 @@
 sudo apt-get update
 
 # Install Apache2
-sudo apt-get install apache2 -y
+# sudo apt-get install apache2 -y
 
 # Enable Apache2 service to start on boot
-sudo systemctl enable apache2.service
+# sudo systemctl enable apache2.service
 
 # Start Apache2 service
-sudo systemctl start apache2.service
+# sudo systemctl start apache2.service
 
-sudo echo "Hello World from $(hostname -f)" >/var/www/html/index.html
+# sudo echo "Hello World from $(hostname -f)" >/var/www/html/index.html
 
 echo "Installing Docker"
 sudo apt-get -y install docker.io docker-compose &&
@@ -22,7 +22,5 @@ sudo apt-get -y install docker.io docker-compose &&
 
 echo "Cloning repo and building docker image"
 git clone https://github.com/hencho108/spotify-realtime-data-streaming.git &&
-    cd spotify-realtime-data-streaming/spotify-producer &&
-    docker build -t spotify-producer .
-
-docker run -it spotify-producer
+    cd spotify-realtime-data-streaming/spotify-producer
+# docker build -t spotify-producer .
