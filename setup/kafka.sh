8926c07451c42d4358ffa47362714fabd78048fc
Hendrik <you@example.com>
2023-04-01 20:33:46 +0200

    improved kafka setup script

--- setup/kafka.sh
+++ setup/kafka.sh
@@ -1,26 +1,22 @@
 #!/bin/bash
+export REPO_URL="https://github.com/hencho108/spotify-realtime-data-streaming.git"
 
-# Update the package list
+echo "Update package list"
 sudo apt-get update
 
-# Install Apache2
-# sudo apt-get install apache2 -y
-
-# Enable Apache2 service to start on boot
-# sudo systemctl enable apache2.service
-
-# Start Apache2 service
-# sudo systemctl start apache2.service
-
-# sudo echo "Hello World from $(hostname -f)" >/var/www/html/index.html
-
 echo "Installing Docker"
-sudo apt-get -y install docker.io docker-compose &&
-    sudo newgrp docker &&
-    docker --version &&
-    docker-compose --version
+sudo apt-get -y install docker.io docker-compose
 
-echo "Cloning repo and building docker image"
-git clone https://github.com/hencho108/spotify-realtime-data-streaming.git &&
-    cd spotify-realtime-data-streaming/spotify-producer
-# docker build -t spotify-producer .
+echo "Adding user to docker group"
+sudo newgrp docker
+
+echo "Cloning repo"
+git clone "$REPO_URL"
+
+echo "Building Kafka docker images"
+cd spotify-realtime-data-streaming/kafka
+docker-compose up -d
+
+echo "Building spotify-producer docker image"
+cd ../spotify-producer
+docker build -t spotify-producer .
